b6485821b81c67f97ec7f67625b74d204885755d
LE GEAR            COLM <c15148823@NANCYS-21.csis.ul.ie>
2018-04-23 15:48:39 +0100

    Full marks for lab 11

--- week11/mpow.cc
+++ week11/mpow.cc
@@ -34,18 +34,18 @@ void multiplyMatrices(const Matrix& first, const Matrix& second, Matrix& result)
 double multiplyRows(const Row& first, const Row& second);
 Matrix createIdentityMatrix(const int size);
 double dotProd(const Row& r1, const Row& r2);
-
+void matrixPow(const Matrix& inMatrix, Matrix& result, int power);
 
 int transposedRowCount = 0;
 
 int main(int argc, char *argv[]) {
 
-    
+
     if(argc < 2) {
         cout << "Illegal exponent; exiting." << endl;
         return 1;
     }
-    
+
     std::istringstream ss(argv[1]);
     int power;
     if (!(ss >> power) || power < 0) {
@@ -55,25 +55,27 @@ int main(int argc, char *argv[]) {
 
     Matrix sparseMatrix;
     readMatrix(sparseMatrix);
-    
+
     if(power == 0) {
         print(createIdentityMatrix(sparseMatrix.size()));
         return 0;
     }
-    
+
     if(power == 1) {
         print(sparseMatrix);
         return 0;
     }
-    
-    Matrix resultMatrix;
-    multiplyMatrices(sparseMatrix, sparseMatrix, resultMatrix);
 
-    for (int j = 2; j < power; j++) {
-        Matrix newResult;
-        multiplyMatrices(sparseMatrix, resultMatrix, newResult);
-        resultMatrix = newResult;
-    }
+    Matrix resultMatrix;
+
+    matrixPow(sparseMatrix, resultMatrix, power);
+    //multiplyMatrices(sparseMatrix, sparseMatrix, resultMatrix);
+
+    //for (int j = 2; j < power; j++) {
+    //    Matrix newResult;
+    //    multiplyMatrices(sparseMatrix, resultMatrix, newResult);
+    //    resultMatrix = newResult;
+    //}
 
     print(resultMatrix);
 
@@ -127,6 +129,22 @@ void transposeMatrix(const Matrix& matrix, Matrix& transposed) {
     }
 }
 
+void matrixPow(const Matrix& inMatrix, Matrix& result, int power) {
+	result = inMatrix;
+	while(power > 1) {
+        Matrix newResult;
+		if(power % 2 == 0 && (power / 2) > 1) {
+			multiplyMatrices(result, result, newResult);
+			power /= 2;
+		} else {
+			multiplyMatrices(inMatrix, result, newResult);
+			power--;
+        }
+        result = newResult;
+	}
+
+}
+
 void multiplyMatrices(const Matrix& first, const Matrix& second, Matrix& result) {
     result.clear();
     for (int i = 0; i < first.size(); i++) {
@@ -136,7 +154,7 @@ void multiplyMatrices(const Matrix& first, const Matrix& second, Matrix& result)
 
     Matrix secondT(transposedRowCount);
     transposeMatrix(second, secondT);
-    
+
     for (int row = 0; row < first.size(); row++) {
         for (int column = 0; column < secondT.size(); column++) {
             double value = multiplyRows(first[row], secondT[column]);
@@ -155,7 +173,7 @@ double multiplyRows(const Row& first, const Row& second) {
     double result = 0.0;
     Row::const_iterator firstPos = first.begin();
     Row::const_iterator secondPos = second.begin();
-    
+
     Row::const_iterator firstEnd = first.end();
     Row::const_iterator secondEnd = second.end();
 
@@ -166,25 +184,25 @@ double multiplyRows(const Row& first, const Row& second) {
             firstPos++;
 
         if(firstPos == firstEnd) break;
-	
+
         while(secondPos->column < targetCol && secondPos != secondEnd)
             secondPos++;
 
         if(secondPos == secondEnd) break;
-	
+
         if (firstPos->column == secondPos->column) {
             result += (firstPos->value * secondPos->value);
             firstPos++;
             if(firstPos == firstEnd) break;
         }
     }
-    
+
     return result;
 }
 
 Matrix createIdentityMatrix(const int size) {
     Matrix identity = Matrix(size);
-    
+
     for(int i = 0; i < size; i++) {
         identity[i].push_back(RowEntry((i + 1), 1));
     }
